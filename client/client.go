f82a16d415c1fc6d787fec2edb4aa2517a77bcac
Samarth Mayya <samarthmayya@gmail.com>
2021-08-10 13:14:12 +0530

    Completed RPC Client and Server setup on port:10000 Added stubs for RPC service Squashed-commit for init-rpc-service

---
+++ client/client.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+        "google.golang.org/grpc/examples/data"
+	pb "example.com/cache-service/cache"
+)
+
+var (
+        tls                = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+        caFile             = flag.String("ca_file", "", "The file containing the CA root cert file")
+        serverAddr         = flag.String("server_addr", "localhost:10000", "The server address in the format of host:port")
+        serverHostOverride = flag.String("server_host_override", "x.test.youtube.com", "The server name used to verify the hostname returned by the TLS handshake")
+)
+
+
+func main() {
+	flag.Parse()
+        var opts []grpc.DialOption
+        if *tls {
+                if *caFile == "" {
+                        *caFile = data.Path("x509/ca_cert.pem")
+                }
+                creds, err := credentials.NewClientTLSFromFile(*caFile, *serverHostOverride)
+                if err != nil {
+                        log.Fatalf("Failed to create TLS credentials %v", err)
+                }
+                opts = append(opts, grpc.WithTransportCredentials(creds))
+        } else {
+                opts = append(opts, grpc.WithInsecure())
+        }
+
+        opts = append(opts, grpc.WithBlock())
+        conn, err := grpc.Dial(*serverAddr, opts...)
+        if err != nil {
+                log.Fatalf("fail to dial: %v", err)
+        }
+        defer conn.Close()
+	client := pb.NewCacheServiceClient(conn)
+	cacheentry := &pb.CacheEntry{Val: &pb.Value{Value: []byte("Bello")}, Key: &pb.Key{Key: "Hello"}}
+	_, err = client.Set(context.Background(), cacheentry)
+	if err != nil {
+		log.Printf("An error has been encountered: %v",err)
+	}
+
+	key := &pb.Key{Key: "Bello"}
+	val, err := client.Get(context.Background(), key)
+	if err != nil {
+		log.Fatalf("An error has occurred while fetching: %v",err)
+	}
+	log.Println(val)
+}
+
+
---
+++ go.mod
@@ -0,0 +1,10 @@
+module example.com/cache-service
+
+go 1.16
+
+require (
+	github.com/golang/protobuf v1.5.2
+	google.golang.org/grpc v1.39.1
+	google.golang.org/grpc/examples v0.0.0-20210810031557-01bababd8349
+	google.golang.org/protobuf v1.27.1
+)
